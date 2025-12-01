f7579f9bd7cc7b214b4546ce57a34c8a46edb44d
ИСП-31 <-31@LAB44-13>
2025-12-01 14:28:52 +0300

    Добавьте файлы проекта.

---
+++ VIPKS_3/MainWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using VIPKS_3.ModelsDB;
+
+namespace VIPKS_3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Student _currentStudent;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadDBinDataGrid();
+        }
+
+        private void LoadDBinDataGrid()
+        {
+            using (StudentsContext _db = new StudentsContext())
+            {
+                int selectedIndex = dg_Students.SelectedIndex;
+                dg_Students.ItemsSource = _db.Students.ToList();
+
+                if (selectedIndex != -1)
+                {
+                    if (selectedIndex >= dg_Students.Items.Count) selectedIndex = dg_Students.Items.Count - 1;
+
+                    dg_Students.SelectedIndex = selectedIndex;
+
+                    dg_Students.ScrollIntoView(dg_Students.SelectedItem);
+                }
+
+                dg_Students.Focus();
+            }
+        }
+
+        private void btn_Add_Click(object sender, RoutedEventArgs e)
+        {
+            gb_AddEditForm.Visibility = Visibility.Visible;
+
+            _currentStudent = null;
+
+            MainWindow f = this;
+            f.DataContext = _currentStudent;
+        }
+
+        private void btn_Edit_Click(object sender, RoutedEventArgs e)
+        {
+            if (dg_Students.SelectedIndex != -1)
+            {
+                gb_AddEditForm.Visibility = Visibility.Visible;
+
+                _currentStudent = ((Student)dg_Students.SelectedItem);               
+
+                MainWindow f = this;
+                f.DataContext = _currentStudent;
+            }           
+        }
+
+        private void btn_Delete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_Refresh_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_ClearSearch_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_Save_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            gb_AddEditForm.Visibility = Visibility.Hidden; 
+
+        }
+
+        private void tb_Search_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void cb_Filter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void dg_Students_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ VIPKS_3/ModelsDB/StudentsContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace VIPKS_3.ModelsDB;
+
+public partial class StudentsContext : DbContext
+{
+    public StudentsContext()
+    {
+    }
+
+    public StudentsContext(DbContextOptions<StudentsContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=localhost\\sqlexpress; Database=students; User=исп-31; Password=1234567890; Encrypt=false");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.ToTable("students");
+
+            entity.Property(e => e.AdmissionDate).HasColumnType("datetime");
+            entity.Property(e => e.FullName)
+                .HasMaxLength(70)
+                .IsUnicode(false);
+            entity.Property(e => e.Group)
+                .HasMaxLength(10)
+                .IsUnicode(false);
+            entity.Property(e => e.StudyForm)
+                .HasMaxLength(7)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
